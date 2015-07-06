51622832ba66cd5d8d2a86f54ece33d48a5e33f0
DealerOnClass <alberto@dealeron.com>
2015-07-06 14:13:54 -0400

    Overhaul v1

--- js/jquery.functions.js
+++ js/jquery.functions.js
@@ -1,15 +1,51 @@
-// https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        // return false;
-      }
+  var pageUrl = window.location.hash.substr(1);
+
+  $('.nav__link').each(function() {
+
+    var navUrl = $(this).attr('href').substr(1);
+
+    if ( pageUrl === navUrl ) {
+      $(this).addClass('active');
     }
+
+    $(this).click(function() {
+        $(this).addClass('active');
+        $(this).parent().siblings().children().removeClass('active');
+    });
+
   });
+
 });
+// (function () {
+//     // get page url after the hash
+//     var pageUrl = window.location.hash.substr(1);
+//     console.log(pageUrl);
+
+//     // get nav url
+//     var navItems = document.getElementsByClassName("nav__link");
+//     for (i = 0; i < navItems.length; i++) {
+
+//         var elem = navItems[i];
+
+//         // get nav url
+//         navUrl = elem.getAttribute("href").substr(1);
+//         console.log(navUrl);        
+
+//         // add class if page url is nav url
+//         if (pageUrl == navUrl) {
+//             navItems[i].setAttribute("class", "active");
+//             console.log("hallelujah!");
+//         };
+
+//         // add click event
+//         // elem.addEventListener("click", function(){
+//         //     this.setAttribute("class", "active");
+//         //     // get this parent
+//         //     // get this siblings
+//         //     // remove active class, done
+//         // });
+        
+//     };
+
+// })();
