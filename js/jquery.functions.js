5fcfaba43da57f7ce419aba9b30ed187a99ce88d
DealerOnClass <alberto@dealeron.com>
2015-07-31 10:57:37 -0400

    responsifty
    i should trade mark that

--- js/jquery.functions.js
+++ js/jquery.functions.js
@@ -13,7 +13,6 @@ $(function() {
   // for each nav
   // var nav__link = $("[data-target='link']");
 
-
   $('[data-target="nav__isActive"]').each(function() {
     // get href
     var navUrlRaw = $(this).attr('href').substr(1);
@@ -37,16 +36,8 @@ $(function() {
         // add active class
         // remove active class of siblings on nav click
         $(this).parent().addClass('active').siblings().removeClass('active');
-
-        // change title on topics page
-        // $('[data-target="title__isActive"]').text(navUrl);
-        // $('.catbloc#' + navUrl + '').siblings().hide();
-        // $('.catbloc#' + navUrl + '').show();
-        // console.log(navUrl);
-        // e.preventDefault();
     });
   });
-
 
   // count total posts in lessons page
   var lesson__category = $(".count__category").attr("data-category");
@@ -60,114 +51,7 @@ $(function() {
   $(".count__siblings").html(lesson__siblings);
   $(".count__category").html(lesson__count);
 
-
-
-
-
-
-
-  // //  Scroll
-  // var heroTop = $(".lessons .hero").offset().top;
-  // // console.log(heroTop);
-
-  // $(document).scroll(function() {
-
-  //   var distanceTop = $(document).scrollTop();
-  //   // console.log(distanceTop);
-
-  //   if(distanceTop > heroTop) {
-  //       console.log("Animate Header Start");
-  //   } else {
-  //       console.log("Animate Header Stop");
-  //   }
-
-  // });
-
 });
-
-
-// https://github.com/codrops/AnimatedHeader/blob/master/js/classie.js
-// var cbpAnimatedHeader = (function() {
-
-//   var docElem = document.documentElement,
-//     header = document.querySelector( '.lessons .layout__hero' ),
-//     didScroll = false,
-//     changeHeaderOn = 300;
-
-//   function init() {
-//     window.addEventListener( 'scroll', function( event ) {
-//       if( !didScroll ) {
-//         didScroll = true;
-//         setTimeout( scrollPage, 250 );
-//       }
-//     }, false );
-//   }
-
-//   function scrollPage() {
-//     var sy = scrollY();
-//     if ( sy >= changeHeaderOn ) {
-//       classie.add( header, 'isSticky' );
-//     }
-//     else {
-//       classie.remove( header, 'isSticky' );
-//     }
-//     didScroll = false;
-//   }
-
-//   function scrollY() {
-//     return window.pageYOffset || docElem.scrollTop;
-//   }
-
-//   init();
-
-// })();
-
-
-  // // change title on topics page on page load
-  // $('.topics .lesson__header [data-target="title__isActive"]').text(hashUrl);
-  // // in case of page refresh remove ghost title to avoid ugliness  
-
-  // // on click
-  // $('[data-target="lesson__isActive"]').click(function(e) {
-  //   // get href
-  //   var navUrl = $(this).attr('href').split('#')[1];
-  //   // add active class
-  //   // remove active class of siblings
-  //   $('.filter__body [data-target="nav__isActive"][href="#' +  navUrl + '"]').addClass('active').siblings().removeClass('active');
-  //   // change title on topics page on tag click
-  //   $('[data-target="title__isActive"]').text(navUrl);
-
-  //   // e.preventDefault();
-  // });
-
-
-  // remedy for seomthign stupid on homepage - REVISIT
-  // $(".category").next().remove()
-
-  
-  // group progress
-  // $('[data-target="group__progress"]').each(function() {
-
-  //   var group__class = $(this).attr("class");
-  //   var group__count = $('[data-target="' + group__class + '__count"]');
-
-  //   var post__count = group__count.closest(".sidebar__item").prevAll().length;
-  //   var post__count = post__count - 1;
-
-  //   var post__total = group__count.closest(".sidebar__item").siblings().length;
-  //   var post__total = post__total - 1;
-
-  //   var group__progress = post__count / post__total * 100 + "%";
-
-  //   $(this).width(group__progress);
-
-  // });
-
-  // frustrating momment with this one
-  // console.log($('#check').children().length);
-  // if( $('#check').children().length == 0  ) {
-  //     $("#check").remove();
-  // };
 
 
 // bootstrap collapse icon switch
@@ -234,13 +118,3 @@ else {
 
   console.log("keep it closed");
 }
-  
-  // make sure topics page always shows something
-  // var topics__url = $('.nav--primary .nav__link[href*="topics"]').attr("href");
-  // var topics__url = topics__url + "#all";
-  // $('.nav--primary .nav__link[href*="topics"]').attr("href", topics__url);
-
-  // $(".catbloc#all").siblings().hide();
-
-  // $(".catbloc#all").siblings().hide();
-  
